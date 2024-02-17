8af8fc3581afb1877f276d905db5a8b952685556
Bean0of <rockingbeargaming@gmail.com>
2024-02-17 15:01:36 -0800

    init

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@ const std::set<ghc::filesystem::path> SUPPORTED_FILE_EXTENSIONS = {
 
 #if defined(GEODE_IS_WINDOWS)
 constexpr uintptr_t DEATH_SOUND_OFFSET = 0x2E6B87;
+#elif defined(GEODE_ANDROID64)
+constexpr uintptr_t DEATH_SOUND_OFFSET = 0x0;
 #elif defined(GEODE_IS_ANDROID32)
 constexpr uintptr_t DEATH_SOUND_OFFSET = 0x306DA5;
 #else
