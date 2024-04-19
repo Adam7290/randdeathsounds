e458002f6506501419f9f3cecd369a653b3b809d
Adam7290 <rockingbeargaming@gmail.com>
2024-04-19 13:32:50 -0700

    v1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 ## Death Sound Randomizer Changelog
+# v1.1.0
+- Moved sounds folder to config dir (pencil icon thing) (your folder will be automatically moved!)
+- Sounds are cached and preloaded when loading a level (no more lagspikes when playing a long sound for the first time!)
+- Now using std::mt19937 rng instead of rand() (aka less repeated sounds)
+
 # v1.0.0
 - Initial release
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-// This mod WILL break when geometry dash updates as it modifies a memory address
-// I completely understand if this mod gets rejected because of that
-
 #include <Geode/Geode.hpp>
-#include <Geode/modify/PlayerObject.hpp>
+#include <random>
+#include "folder_manager.h"
+
+using namespace geode::prelude;
 
 const char* DEFAULT_DEATH_SOUND = "explode_11.ogg";
-const std::set<ghc::filesystem::path> SUPPORTED_FILE_EXTENSIONS = {
+const std::set<fs::path> SUPPORTED_FILE_EXTENSIONS = {
 	".ogg",
 	".mp3",
 	".wav",
@@ -13,45 +13,48 @@ const std::set<ghc::filesystem::path> SUPPORTED_FILE_EXTENSIONS = {
 
 #if defined(GEODE_IS_WINDOWS)
 constexpr uintptr_t DEATH_SOUND_OFFSET = 0x2E6B87;
+#elif defined(GEODE_IS_ANDROID64)
+constexpr uintptr_t DEATH_SOUND_OFFSET = 0x006bab78;
 #else
 #error Undefined death sound offset for platform.
 #endif
 
-using namespace geode::prelude;
-
 char* deathSound = new char[4096];
-
-ghc::filesystem::path getFolder() {
-	return Mod::get()->getSaveDir()/"deathsounds";
-}
+std::vector<gd::string> soundPool;
+std::mt19937 rng;
 
 void updateSound() {
-	log::info("Updating death sound.");
-
-	if (!Mod::get()->getSettingValue<bool>("enabled")) {
-		strcpy(deathSound, DEFAULT_DEATH_SOUND);
-		return;
-	}
-
-	std::vector<std::string> filePool;
-	for (const auto& file : ghc::filesystem::directory_iterator(getFolder())) {
-		if (SUPPORTED_FILE_EXTENSIONS.find(file.path().extension()) != SUPPORTED_FILE_EXTENSIONS.end()) {
-			filePool.push_back(file.path().string());
-		}
-	}
-	
-	if (!filePool.empty()) {
-		const char* randSound = filePool[rand()%filePool.size()].c_str();
-		log::info("Randomly chosen sound: {}", randSound);
+	if (Mod::get()->getSettingValue<bool>("enabled") && !soundPool.empty()) {
+		const char* randSound = soundPool[std::uniform_int_distribution<int>(0, soundPool.size()-1)(rng)].c_str();
 		strcpy(deathSound, randSound);
 	} else {
-		log::info("Using default sound because folder is empty or doesn't have any valid sounds in it.");
 		strcpy(deathSound, DEFAULT_DEATH_SOUND);
 	}
 }
 
+#include <Geode/modify/PlayLayer.hpp>
+class $modify(PlayLayer) {
+	$override bool init(GJGameLevel* p0, bool p1, bool p2) {
+		if (!PlayLayer::init(p0, p1, p2)) {
+			return false;
+		}
+
+		soundPool.clear();
+		for (const auto& file : fs::directory_iterator(Folder::getFolder())) {
+			if (SUPPORTED_FILE_EXTENSIONS.find(file.path().extension()) != SUPPORTED_FILE_EXTENSIONS.end()) {
+				gd::string sound = file.path().string();
+				soundPool.push_back(sound);
+				FMODAudioEngine::sharedEngine()->preloadEffect(sound);
+			}
+		}
+
+		return true;
+	}
+};
+
+#include <Geode/modify/PlayerObject.hpp>
 class $modify(PlayerObject) {
-	void playerDestroyed(bool p) {
+	$override void playerDestroyed(bool p) {
 		if (!p) {
 			updateSound();
 		}
@@ -61,18 +64,17 @@ class $modify(PlayerObject) {
 };
 
 $execute {
-	// Create folder if it doesnt exist
-	auto folder = getFolder();
-	if (!ghc::filesystem::exists(folder)) {
-		ghc::filesystem::create_directory(folder);
-	}
+	// Create death sounds folder
+	Folder::setup();
 
-	srand(time(NULL)); // Seed rng
+	// Seed rng
+	rng.seed(time(NULL));
 	
 	// Make push instructions push our deathSound var instead of gd "explode_11.ogg" string
-	log::info("Patching death sound");
+	const auto address = reinterpret_cast<void*>(geode::base::get() + DEATH_SOUND_OFFSET);
+	log::info("Patching death sound (0x{:X})", (uintptr_t)address);
 	if (Mod::get()->patch(
-		reinterpret_cast<void*>(geode::base::get() + DEATH_SOUND_OFFSET),
+		address,
 		geode::toByteArray(deathSound)
 	).isErr()) {
 		log::error("Failed to patch death sounds");
